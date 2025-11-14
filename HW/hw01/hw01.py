94b9936621a585c4a0ba5fcec02593c5651e9b93
rengran233 <2052751232@qq.com>
2025-11-14 19:03:39 +0800

    hw01 accomplished

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,8 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    # max() supports multiple arguments
+    return i * i + j * j + k * k - max(i, j, k) * max(i, j, k)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +67,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    # from n-1 to 1
+    for i in range(n - 1, 0, -1):
+        if n % i == 0:
+            return i
+    return 1
 
 
 def hailstone(n):
@@ -88,4 +94,15 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+
+    cnt = 1
+    while n != 1:
+        print(n)
+        cnt += 1
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+    print(1)
+    return cnt
 
